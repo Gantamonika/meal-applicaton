a938c0956e62cede1398cb9f549100be92148102
Monika <gantamonika1998@gmail.com>
2022-03-24 10:41:05 +0530

    Home commit

---
+++ src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>    
+        <div>
+              <button class="btn" @click="getList">Get Meal</button>
+        </div>    
+              <ul >
+                <li v-for="item in recipes" :key="item">
+                     {{ item.strCategory }} </li>
+                   
+            </ul>
+    </div>
+  
+</template>
+<script>
+import Axios from 'axios';
+export default {
+    name: 'Home',
+
+ data() {
+    return {
+      meal: "",
+      recipes: '',
+      // youtubeVid: ""
+    };
+  },
+//   created(){
+//     this.getList();
+//   },
+  methods: {
+    getList() {
+      Axios.get("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
+        .then(response => {
+            console.log(response.data.meals);
+          this.recipes = response.data.meals;
+         
+          // this.youtubeVid = [data.body.meals[0].strYoutube];
+        });
+    },
+  },
+
+
+};
+</script>
+<style>
+
+</style>
