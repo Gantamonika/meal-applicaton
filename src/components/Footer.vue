f40c3a15d7b525977be582353c3a265f7b06ea80
Monika <gantamonika1998@gmail.com>
2022-04-12 17:08:13 +0530

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
 </head>
 <footer id="footer" class="bg-primary text-white d-flex-column text-center">
